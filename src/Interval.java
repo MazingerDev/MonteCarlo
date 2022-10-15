2152480b948cc75ed7412a9aff9d7491e2653010
MazingerDev <mazinasd7@gmail.com>
2022-10-15 20:36:50 +0200

    add interval class

--- src/Interval.java
+++ src/Interval.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Interval {
+import java.util.ArrayList;
+
+public class Interval  {
+
+
+    private ArrayList<Integer> result = new ArrayList<Integer>();
+
+
+    public ArrayList<Integer> getInterval()
+    {
+
+        return result;
+    }
+
+    public void setInterval(ArrayList<Double> cumulative)
+    {
+        result.add(1);
+        for (Double aDouble : cumulative) {
+            result.add((int) (aDouble * 100));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
