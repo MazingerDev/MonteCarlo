4ec858e45536b0b98f0ab0100ca267021076d7c7
MazingerDev <mazinasd7@gmail.com>
2022-10-15 20:37:30 +0200

    add simulation random test

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Simulation {
+import  java.lang.Math;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Simulation {
+
+    private Random random = new Random();
+
+
+    private ArrayList<Integer> simulation = new ArrayList<Integer>();
+    private ArrayList<Integer> randomList = new ArrayList<Integer>();
+
+    public ArrayList<Integer> simulation(int timesOfSimulatoin,ArrayList<Integer> interval, ArrayList<Double> demand)
+    {
+        for(int n=0 ; n<timesOfSimulatoin; n++)
+        {
+            Integer ran = random.nextInt(100);
+            randomList.add(ran);
+
+        }
+
+        for(int n= 0; n< timesOfSimulatoin;n++)
+        {
+            for (int i = 0; i < interval.size() - 1; i++) {
+                if (interval.get(i) < randomList.get(n) && interval.get(i + 1) > randomList.get(n)) {
+                    double tmp = demand.get(i);
+                    simulation.add((int) tmp);
+
+                }
+            }
+        }
+        return simulation;
+    }
+
+
+    public ArrayList<Integer> getRandomList() {
+        return randomList;
+    }
+
+
 }
