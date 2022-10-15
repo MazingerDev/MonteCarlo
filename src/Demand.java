2a45773821878c792d696cbf8519365f57f0f434
MazingerDev <mazinasd7@gmail.com>
2022-10-15 20:38:09 +0200

    add attribute  to return demand/class list

--- src/Demand.java
+++ src/Demand.java
@@ -8,6 +8,10 @@ public class Demand {
         this.demandPerFrequency.add(demandPerFrequency);
         demand += demandPerFrequency;
     }
+    public ArrayList<Double> getDemandPerFrequency()
+    {
+        return demandPerFrequency;
+    }
 
 
 }
