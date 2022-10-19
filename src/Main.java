8861dd2df7897113bf866df71c46ee37febb1f02
MazingerDev <mazinasd7@gmail.com>
2022-10-20 01:15:03 +0200

    customise cli

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         Demand demand = new Demand();
         Frequency frequency = new Frequency();
@@ -9,8 +10,15 @@ public class Main {
         CumulativeProbaility cumulative = new CumulativeProbaility();
         Interval interval = new Interval();
         Simulation simulation = new Simulation();
+        CommandLineTable table = new CommandLineTable();
+        CommandLineTable table1 = new CommandLineTable();
+
+        table.clearConsole();
+        System.out.print("Enter the table size : ");
+
         int timesOfInputs = input.nextInt();
 
+        System.out.println("Enter the table (class frequency)");
         for(int counter =0;counter<timesOfInputs;counter++)
         {
             double currentDemand = input.nextDouble();
@@ -19,19 +27,43 @@ public class Main {
             frequency.setFrequencyPerDemand(currentFrequency);
         }
 
+
+
+
+
         for( int counter = 0 ; counter <timesOfInputs; counter++)
         {
             probaility.probailityOfOccurrence(frequency.getFrequencyPerDemand(counter), frequency.getFrequency());
             cumulative.cumulativeProbaility(probaility.getProbailityOfOccurrence());
         }
         interval.setInterval(cumulative.getCumulativeProbaility());
-        System.out.println(probaility.getProbailityOfOccurrence());
-        System.out.println(cumulative.getCumulativeProbaility());
-        System.out.println(interval.getInterval());
-        System.out.println(simulation.simulation(10,interval.getInterval(),demand.getDemandPerFrequency()));
-        System.out.println(simulation.getRandomList());
 
 
+        table.clearConsole();
+
+        table.setShowVerticalLines(true);
+
+        table.setHeaders("Class", "Frequency","Probability of occurrence","Cumulative","Interval of random numbers");
+        for(int counter=0 ; counter<timesOfInputs; counter++)
+        {
+            table.addRow(String.valueOf(demand.getDemandPerFrequency().get(counter)), String.valueOf(frequency.getFrequencyPerDemand(counter)), String.valueOf(probaility.getProbailityOfOccurrence().get(counter)), String.valueOf(cumulative.getCumulativeProbaility().get(counter)),interval.getIntervalList().get(counter));
+
+        }
+        table.print();
+
+        System.out.print("Enter the number of simulation time : ");
+        int timesOfSimulation = input.nextInt();
+
+        table1.setShowVerticalLines(true);
+        table1.setHeaders("Simulation time","Random numbers","Simulation values");
+
+        for(int counter =0 ; counter<timesOfSimulation; counter++)
+        {
+            int sim = simulation.simulation(timesOfSimulation,interval.getInterval(),demand.getDemandPerFrequency()).get(counter);
+            table1.addRow(String.valueOf(counter+1), String.valueOf(simulation.getRandomList().get(counter)),String.valueOf(sim));
+        }
+        table1.print();
+        System.out.println("\n"+"Simulation average : "+ simulation.getAverage(timesOfSimulation));
 
 
 
