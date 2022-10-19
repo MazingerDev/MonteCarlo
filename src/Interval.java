55f0c0d5a28e458a66064468819c798612014335
MazingerDev <mazinasd7@gmail.com>
2022-10-20 01:13:29 +0200

    add interval range

--- src/Interval.java
+++ src/Interval.java
@@ -4,6 +4,7 @@ public class Interval  {
 
 
     private ArrayList<Integer> result = new ArrayList<Integer>();
+    private ArrayList<String> interval = new ArrayList<String>();
 
 
     public ArrayList<Integer> getInterval()
@@ -14,11 +15,29 @@ public class Interval  {
 
     public void setInterval(ArrayList<Double> cumulative)
     {
-        result.add(1);
+
+        double firstIndex = cumulative.get(0) *100;
+        if((int) firstIndex !=1 )
+        {
+            result.add(1);
+
+        }
         for (Double aDouble : cumulative) {
-            result.add((int) (aDouble * 100));
+            result.add((int) (aDouble * 100)+1);
+        }
+
+        for(int index =0 ; index < result.size()-1; index++)
+        {
+           interval.add(String.valueOf(result.get(index))+" to " +String.valueOf(result.get(index+1)-1));
+
         }
     }
+
+    public ArrayList<String> getIntervalList()
+    {
+        return interval;
+    }
+
 
 
 
